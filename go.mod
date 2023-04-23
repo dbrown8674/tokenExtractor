3526469b5a660a8648a70cad1a5fe552f7a284bb
Duncan <dbrown8674@gmail.com>
2023-04-23 17:53:04 -0400

    Add claim validation Switch where sanitized headers come from

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module tokenExtractor
 go 1.18
 
 require (
+	github.com/fatih/structs v1.1.0
 	github.com/golang-jwt/jwt v3.2.2+incompatible
 	github.com/tetratelabs/proxy-wasm-go-sdk v0.22.0
 )
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"errors"
 	"fmt"
+	"github.com/fatih/structs"
 	"github.com/golang-jwt/jwt"
 	"github.com/tetratelabs/proxy-wasm-go-sdk/proxywasm"
 	"github.com/tetratelabs/proxy-wasm-go-sdk/proxywasm/types"
@@ -42,8 +43,8 @@ type httpAuthRandom struct {
 	contextID uint32
 }
 
-func (ctx *httpAuthRandom) OnHttpRequestHeaders(numHeaders int, _ bool) types.Action {
-	for k, _ := range sanitizedHeaders {
+func (ctx *httpAuthRandom) OnHttpRequestHeaders(_ int, _ bool) types.Action {
+	for k := range sanitizedHeaders {
 		err := proxywasm.RemoveHttpRequestHeader(k)
 		if err != nil {
 			proxywasm.LogErrorf("error sanitizing headers %v", err)
@@ -64,15 +65,17 @@ func (ctx *httpAuthRandom) OnHttpRequestHeaders(numHeaders int, _ bool) types.Ac
 		proxywasm.LogErrorf("error extracting token %v", err)
 		return types.ActionContinue
 	}
-	claims, err := decodeToken(tokenString, keySetCache)
+	claims := &jwt.StandardClaims{}
+	token, err := decodeToken(tokenString, claims, keySetCache)
 	if err != nil {
 		proxywasm.LogErrorf("error decoding token %v", err)
 		return types.ActionContinue
 	}
+	claimMap := structs.Map(token.Claims)
 	for k, v := range sanitizedHeaders {
-		err = proxywasm.AddHttpRequestHeader(k, fmt.Sprint(claims.Header[v]))
+		err = proxywasm.AddHttpRequestHeader(k, fmt.Sprint(claimMap[v]))
 		if err != nil {
-			proxywasm.LogErrorf("error adding header [%s:%s] %v", k, claims.Header[v], err)
+			proxywasm.LogErrorf("error adding header [%s:%s] %v", k, claimMap[v], err)
 		}
 	}
 	return types.ActionContinue
@@ -86,8 +89,8 @@ func extractToken(authHeader string) (string, error) {
 	return parts[1], nil
 }
 
-func decodeToken(tokenString string, keySet map[string]jwk) (*jwt.Token, error) {
-	return jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+func decodeToken(tokenString string, claims *jwt.StandardClaims, keySet map[string]jwk) (*jwt.Token, error) {
+	return jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
 			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
 		}
@@ -98,11 +101,14 @@ func decodeToken(tokenString string, keySet map[string]jwk) (*jwt.Token, error) 
 		if _, ok := keySet[kid]; !ok {
 			return nil, fmt.Errorf("key %v not found", kid)
 		}
+		if err := token.Claims.Valid(); err != nil {
+			return nil, err
+		}
 		return token, nil
 	})
 }
 
-func (ctx *pluginContext) OnPluginStart(pluginConfigurationSize int) types.OnPluginStartStatus {
+func (ctx *pluginContext) OnPluginStart(_ int) types.OnPluginStartStatus {
 	if err := proxywasm.SetTickPeriodMilliSeconds(uint32(tick.Milliseconds())); err != nil {
 		proxywasm.LogCriticalf("failed to set tick period: %v", err)
 		return types.OnPluginStartStatusFailed
